c71fb364f5da37c9f3a37185878952d93073b3aa
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-11-15 15:22:19 +0100

    creati file per progetto

---
+++ src/components/DischiContainer.vue
@@ -0,0 +1,29 @@
+<template>
+<h1>prova</h1>
+    
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "DischiContainer",
+    data() {
+        return { elencoDischi: [], 
+        };
+    },
+
+    mounted() {
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((risposta)=>{
+            this.elencoDischi.push(...risposta.data);
+        });
+    }
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
